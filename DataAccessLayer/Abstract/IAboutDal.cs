1947bbe6b801c7717ca1ec024023dd97e139e8cb
ömer faruk yazıcı <omer-faruk-yazici@outlook.com>
2025-10-03 18:14:05 +0300

    Proje dosyası ekle.

---
+++ DataAccessLayer/Abstract/IAboutDal.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IAboutDal: IGenericDal<About>
+    {
+        /*
+         Artık IGenaricDal'dan miras aldığımız için bu metotlara gerek yok.
+
+        void Insert(About a);
+        void Update(About a);
+        void Delete(About a);
+        List<About> GetListAll();
+        About GetByID(int id);
+
+        */
+
+
+    }
+}
---
+++ DataAccessLayer/Abstract/IContactDal.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IContactDal: IGenericDal<Contact>
+    {
+        /*
+         Artık IGenaricDal'dan miras aldığımız için bu metotlara gerek yok.
+
+        void Insert(Contact c);
+        void Update(Contact c);
+        void Delete(Contact c);
+        List<Contact> GetListAll();
+        Contact GetByID(int id);
+
+        
+        */
+    }
+}
