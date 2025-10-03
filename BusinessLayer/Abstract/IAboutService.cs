cfc569dd93b81271b8f2c91853caa167ca1cc668
ömer faruk yazıcı <omer-faruk-yazici@outlook.com>
2025-10-03 18:26:58 +0300

    Abstract service class ları oluşturuldu

---
+++ BusinessLayer/Abstract/IAboutService.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IAboutService : IGenericService<About>
+    {
+        //IGenericService'den miras alır ve aşağıdakileri artık ayrı ayrı tanımlamaya gerek yok
+        //void TAdd(About t);
+        //void TDelete(About t);
+        //void TUpdate(About t);
+        //List<About> TGetList();
+        //About TGetByID(int id);
+    }
+}
