3fba419189b38aed6cf1c8ae5b15774b0a7d3993
ömer faruk yazıcı <omer-faruk-yazici@outlook.com>
2025-10-05 14:34:25 +0300

    Tema görselleri düzenlendi, Partial View Kullanıldı, Navbar partial edildi

---
+++ Core_Proje/Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        /* Bu controller, varsayılan (default) sayfa isteklerini yönetir. 
+         * Index metodu, ana sayfa için bir görünüm döndürür.
+         */
+        public IActionResult Index()
+        {
+
+
+
+            return View();
+        }
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult NavbarPartial()
+        {
+            return PartialView();
+        }
+
+
+    }
+}
