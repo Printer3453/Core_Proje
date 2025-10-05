00e1ffa4e462bef89f06c810023ca6756f417f6b
ömer faruk yazıcı <omer-faruk-yazici@outlook.com>
2025-10-05 14:49:28 +0300

    ViewComponents klasörü oluşturulduve içine FeatureList.cs ı  yerleştirildi.

---
+++ Core_Proje/ViewComponents/Feature/FeatureList.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+
+
+namespace Core_Proje.ViewComponents.Feature
+{
+
+
+
+    public class FeatureList: ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+        /* Bu ViewComponent, özelliklerin (features) listelenmesi için kullanılır.
+         * Invoke metodu, ViewComponent'in ana işlevini yerine getirir ve bir görünüm döndürür.
+         * Controller'daki action metotlarına benzer şekilde çalışır.
+         * Amaç, belirli bir işlevselliği kapsülleyip, tekrar kullanılabilir hale getirmektir.
+         * avantajı, kodun daha modüler ve yönetilebilir olmasını sağlamaktır.
+         * Controller'dan farklı olarak, ViewComponent'ler doğrudan bir görünüm içinde çağrılabilir.
+         * Doğrudan bir URL'ye yanıt vermezler, bunun yerine bir sayfanın parçası olarak kullanılırlar.
+         * Controller'lar genellikle tam sayfa yanıtları döndürürken, ViewComponent'ler sayfanın 
+         * belirli bölümlerini oluşturmak için kullanılır.
+         */
+
+
+
+
+
+    }
+}
