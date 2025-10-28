927eb126630cb1c9773e300bf05f5eaa68fb21a5
Ömer Faruk YAZICI <mrfarukyazici@gmail.com>
2025-10-28 22:23:03 +0300

    Admin paneline Deneyimlerim sayfası oluşturuldu. Listeleme, Ekleme Silme, Güncelleme özellikleri eklendi.

--- Core_Proje/Controllers/ExperienceController.cs
+++ Core_Proje/Controllers/ExperienceController.cs
@@ -1,16 +1,57 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
 {
     public class ExperienceController : Controller
     {
-        ExperienceManager experianceManager = new ExperienceManager(new EfExperienceDal());
+        ExperienceManager _experienceManager = new ExperienceManager(new EfExperienceDal());
         public IActionResult Index()
         {
-            var values = experianceManager.TGetList();
+            ViewBag.v1 = "Deneyim Lİstesi ";
+            ViewBag.v2 = "Deneyimler";
+            ViewBag.v3 = "Deneyim Listesi ";
+            var values = _experienceManager.TGetList();
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddExperience()
+        {
+            ViewBag.v1 = "Deneyim Ekleme ";
+            ViewBag.v2 = "Deneyimler ";
+            ViewBag.v3 = "Deneyim Ekleme ";
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddExperience(Experience experience)
+        {
+            
+            _experienceManager.TAdd(experience);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteExperience(int id)
+        {
+            var value = _experienceManager.TGetByID(id);
+            _experienceManager.TDelete(value);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateExperience(int id)
+        {
+            ViewBag.v1 = "Deneyim Güncelleme ";
+            ViewBag.v2 = "Deneyimler ";
+            ViewBag.v3 = "Deneyim Güncelleme ";
+            var value = _experienceManager.TGetByID(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateExperience(Experience experience)
+        {
+            _experienceManager.TUpdate(experience);
+            return RedirectToAction("Index");
         }
     }
 }
