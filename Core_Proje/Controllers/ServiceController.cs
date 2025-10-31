da0ad0718d11bb3a24551d0408dc171caf5a7bfd
Ömer Faruk YAZICI <mrfarukyazici@gmail.com>
2025-10-31 19:57:50 +0300

    Servis işlemleri tamamlandı(CRUD)

--- Core_Proje/Controllers/ServiceController.cs
+++ Core_Proje/Controllers/ServiceController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
@@ -16,5 +17,44 @@ namespace Core_Proje.Controllers
             return View(values);
         }
 
+
+        [HttpGet]
+        public IActionResult AddService()
+        {
+            ViewBag.v1 = "Hizmet Lİstesi ";
+            ViewBag.v2 = "Hizmetler";
+            ViewBag.v3 = "Hizmet Ekleme ";
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddService(Service service)
+        {
+
+            _serviceManager.TAdd(service);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteService(int id)
+        {
+            var value = _serviceManager.TGetByID(id);
+            _serviceManager.TDelete(value);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditService(int id)
+        {
+            ViewBag.v1 = "Hizmet Lİstesi ";
+            ViewBag.v2 = "Hizmetler";
+            ViewBag.v3 = "Hizmet Güncelleme ";
+
+            var value = _serviceManager.TGetByID(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult EditService(Service service)
+        {
+            _serviceManager.TUpdate(service);
+            return RedirectToAction("Index");
+        }
     }
 }
