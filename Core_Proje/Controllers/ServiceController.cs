531c825babac17ab62b12b9ea7bf3fcf7b723071
Ömer Faruk YAZICI <mrfarukyazici@gmail.com>
2025-10-31 19:21:15 +0300

    admin paneline hakkımda kısmı eklendi, güncelleştirme özellikleri eklendi

---
+++ Core_Proje/Controllers/ServiceController.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class ServiceController : Controller
+    {
+        ServiceManager _serviceManager= new ServiceManager(new EfServiceDal());
+        public IActionResult Index()
+        {
+            var values = _serviceManager.TGetList();
+            return View(values);
+        }
+
+    }
+}
