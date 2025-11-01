a4b576ca40e743ed80737e77785b7cd96235badc
Ömer Faruk YAZICI <mrfarukyazici@gmail.com>
2025-11-02 00:56:15 +0300

    StatisticsDashboard2 adında yeni bir view opuşturuldu, ikinci istatistik tabloları oraya taşındı ve revize edildi

---
+++ Core_Proje/ViewComponents/Dashboard/StatisticsDashboard2.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+    public class StatisticsDashboard2 : ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.portfolios = c.Portfolios.Count();
+            ViewBag.messages = c.Messages.Count();
+            ViewBag.services = c.Services.Count();
+
+            return View();
+        }
+    }
+}
