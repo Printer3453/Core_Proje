623ff384eabf22c9ff019843569eb96c80f4643d
Ömer Faruk YAZICI <mrfarukyazici@gmail.com>
2025-11-01 16:05:17 +0300

    5 projenin Dashboard Last5Projects : ViewComponent da gösterimi

---
+++ Core_Proje/ViewComponents/Dashboard/Last5Projects.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+    public class Last5Projects : ViewComponent
+    {
+
+        PortfolioManager _portfolioManager = new PortfolioManager(new EfPortfolioDal());
+
+        public IViewComponentResult Invoke()
+        {
+            //son 5 projeyi listelemek için
+            var values = _portfolioManager.TGetList().Take(5).ToList();
+            return View(values);
+        }
+    }
+}
