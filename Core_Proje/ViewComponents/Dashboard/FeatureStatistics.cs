48dd6b65e6bc0133dbe3c7250b2572c8831d8b9d
Ömer Faruk YAZICI <mrfarukyazici@gmail.com>
2025-11-01 15:04:09 +0300

    Dashboard da üst menü FeatureStatistics adında bir ViewComponent e taşindi , burada skills, messageFalse,messageTrue,experiences,lastMessageFalse,lastMessageTrue,lastSkill,lastExperience gibi tabloların sayısı ve en son eklenenleri gösterildi

---
+++ Core_Proje/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+    public class FeatureStatistics : ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.skills = c.Skills.Count();
+            ViewBag.messageFalse = c.Messages.Where(x=>x.Status==false).Count();
+            ViewBag.messageTrue = c.Messages.Where(x => x.Status == true).Count();
+            ViewBag.experiences = c.Experiences.Count();
+            ViewBag.lastMessageFalse = c.Messages.Where(x => x.Status == false).OrderByDescending(x=>x.MessageID).Select(y=>y.Name).FirstOrDefault();
+            ViewBag.lastMessageTrue = c.Messages.Where(x => x.Status == true).OrderByDescending(x => x.MessageID).Select(y => y.Name).FirstOrDefault();
+            ViewBag.lastSkill = c.Skills.OrderByDescending(x => x.SkillID).Select(y => y.Title).FirstOrDefault();
+            ViewBag.lastExperience = c.Experiences.OrderByDescending(x => x.ExperienceID).Select(y => y.Name).FirstOrDefault();
+            return View();
+        }
+    }
+}
