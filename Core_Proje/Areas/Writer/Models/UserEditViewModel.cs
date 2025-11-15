5523e8b73b87d0b488ed6a08dbb9e621837fa7b0
Ömer Faruk YAZICI <mrfarukyazici@gmail.com>
2025-11-16 02:18:08 +0300

    User Edit View Model tasarlandı

---
+++ Core_Proje/Areas/Writer/Models/UserEditViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Core_Proje.Areas.Writer.Models
+{
+    public class UserEditViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirm { get; set; }
+        public string ImageUrl { get; set; }
+        public IFormFile Picture { get; set; }
+
+
+
+    }
+}
